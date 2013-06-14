ea538133c797e887dde4b70f2111765785018a57
Rettup <accelee@gmail.com>
2013-06-14 09:33:53 +0800

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,16 +19,23 @@ describe User do
   				            password_confirmation: "foobar")}
   subject { @user}
 
-  it{ should respond_to(:name)}
-  it{ should respond_to(:email)}
-  it{ should respond_to(:password_digest)}
-  it{ should respond_to(:password)}
-  it{ should respond_to(:password_confirmation)}
-  it{ should respond_to(:remember_token)}
-  it{ should respond_to(:authenticate)}
+  it{ should respond_to(:name) }
+  it{ should respond_to(:email) }
+  it{ should respond_to(:password_digest) }
+  it{ should respond_to(:password) }
+  it{ should respond_to(:password_confirmation) }
+  it{ should respond_to(:remember_token) }
+  it{ should respond_to(:authenticate) }
 
 
   it{ should be_valid}
+  it{ should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before { @user.toggle!(:admin)}
+
+    it { should be_admin }
+  end
 
   describe "when name is not present" do
   	before {@user.name = " "}
@@ -125,4 +132,5 @@ describe User do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+
 end
